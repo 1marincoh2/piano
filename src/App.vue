a74cea50849331fad3c8b6e574d5ecbc219f0b8d
1marincoh2 <jose_aries93@hotmail.com>
2021-01-26 18:06:29 -0500

    realiando el piano

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <piano/>
+</template>
+
+<script>
+import piano from './components/piano.vue'
+
+export default {
+  name: 'App',
+  components: {
+    piano
+  }
+}
+</script>
---
+++ src/components/piano.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="row">
+    <div class="col-lg-6 offset-lg-3">
+      <ul class="list-group">
+        <li
+          v-for="(sonido, index) in sonidos"
+          v-bind:key="index"
+          class="list-group-item"
+          v-bind:style="{ 'background-color': sonido }"
+          v-on:click="aplicarSonido(index + 1)"
+        ></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  onMounted,
+  reactive,
+  ref,
+} from "@vue/composition-api";
+
+
+const piano = defineComponent({
+  components: {},
+  setup() {
+    const statepiano = reactive({
+
+       sonidos: [
+       'red', 'orange', 'yellow', 'green', 'teal', 'blue', 'purple'
+      ] 
+          });
+
+
+     function aplicarSonido(index:any){
+             const audio = new Audio();
+            audio.src = 'assets/note' + index + '.wav';
+            audio.load();
+            audio.play();
+    };
+
+ return{
+
+   statepiano,
+   aplicarSonido,
+
+  }
+
+ }
+
+});
+export default piano;
+</script>
+<style scoped>
+.list-group-item {
+  height: 80px;
+  cursor: pointer;
+}
+.list-group-item:active {
+  opacity: 0.7;
+}
+</style>
